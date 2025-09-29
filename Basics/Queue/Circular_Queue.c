7383726ec6e32e61f7ce6efa166b2d3d187f079b
AnshullKumar <anshustudy21@gmail.com>
2025-09-30 00:42:47 +0530

    new commit

---
+++ Basics/Queue/Circular_Queue.c
@@ -0,0 +1,6 @@
+// Simple Menu-Driven Circular Queue Program
+// • Implements a circular queue using an array (fixed size) where the pointer uses formula:(pointer + 1) % Size_of_array to move.
+// • Provides basic operations: Enqueue, Dequeue, Peek (view front), and Display.
+// • Uses a menu loop so the user can repeatedly choose operations.
+// • Handles queue overflow and underflow efficiently by wrapping around the array.
+
