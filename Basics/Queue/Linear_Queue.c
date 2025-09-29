7045ad5e6fa8b57b5e4c1aa11c2dfdff9e41bacb
AnshullKumar <anshustudy21@gmail.com>
2025-09-30 00:30:29 +0530

    Added Peek() and Display()

--- Basics/Queue/Linear_Queue.c
+++ Basics/Queue/Linear_Queue.c
@@ -14,9 +14,9 @@ int front = -1, rear = -1;
 
 void enqueue(int value) {
     if (rear == MAX - 1) {
-        printf("\nQueue Overflow!\n");  // The queue is full
+        printf("\nQueue Overflow!\n");  
     } else {
-        if (front == -1) front = 0;    // First element being added
+        if (front == -1) front = 0;    // Cold start
         queue[++rear] = value;
         printf("%d enqueued into queue.\n", value);
     }
@@ -24,9 +24,28 @@ void enqueue(int value) {
 
 void dequeue() {
     if (front == -1 || front > rear) {
-        printf("\nQueue Underflow!\n");  // There are no elements in the queue
-        front = rear = -1;              // Reset queue when empty
+        printf("\nQueue Underflow!\n");  
+        front = rear = -1;              // Resets the queue when empty
     } else {
         printf("%d dequeued from queue.\n", queue[front++]);
+    }
+}
+
+void peek() {
+    if (front == -1 || front > rear) {
+        printf("\nQueue is empty.\n");
+    } else {
+        printf("\nFront element is: %d\n", queue[front]);
+    }
+}
+
+void display() {
+    if (front == -1 || front > rear) {
+        printf("\nQueue is empty.\n");
+    } else {
+        printf("\nQueue elements (front to rear):\n");
+        for (int i = front; i <= rear; i++) {  // Display from front to rear
+            printf("%d\n", queue[i]);
+        }
     }
 }
