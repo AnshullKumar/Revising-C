71e4b963e27d04af229710b717d434e2ad6ec980
AnshullKumar <anshustudy21@gmail.com>
2025-09-29 15:15:21 +0530

    new commit

---
+++ Basics/Queue/Linear_Queue.c
@@ -0,0 +1,8 @@
+// Simple Menu-Driven Linear Queue Program
+// • Implements a linear queue using an array (fixed size).
+// • Provides basic operations: Enqueue, Dequeue, Peek (view front), and Display.
+// • Uses a menu loop so the user can repeatedly choose operations.
+// • Handles queue overflow (when full) and underflow (when empty).
+
+
+// Written the overview as to wkat  ill be doing next
