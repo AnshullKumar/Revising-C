b7ec79b400b588db11c3eb0de9704a411c492997
AnshullKumar <anshustudy21@gmail.com>
2025-09-30 00:27:05 +0530

    Implemented Enqueue() and Dequeue() in Menu Driven Linear Queue

--- Basics/Queue/Linear_Queue.c
+++ Basics/Queue/Linear_Queue.c
@@ -5,4 +5,28 @@
 // • Handles queue overflow (when full) and underflow (when empty).
 
 
-// Written the overview as to wkat  ill be doing next
+#include <stdio.h>
+
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("\nQueue Overflow!\n");  // The queue is full
+    } else {
+        if (front == -1) front = 0;    // First element being added
+        queue[++rear] = value;
+        printf("%d enqueued into queue.\n", value);
+    }
+}
+
+void dequeue() {
+    if (front == -1 || front > rear) {
+        printf("\nQueue Underflow!\n");  // There are no elements in the queue
+        front = rear = -1;              // Reset queue when empty
+    } else {
+        printf("%d dequeued from queue.\n", queue[front++]);
+    }
+}
