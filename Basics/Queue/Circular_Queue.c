0335b2da1bc1963bf70606bbc9406d0ea1b25edd
AnshullKumar <anshustudy21@gmail.com>
2025-10-01 00:44:23 +0530

    Implement main menu loop for circular queue operations

--- Basics/Queue/Circular_Queue.c
+++ Basics/Queue/Circular_Queue.c
@@ -67,3 +67,41 @@ void display() {
     }
 }
 
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n--- Circular Queue Menu ---\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. View Front\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d", &value);
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
+
+    return 0;
+}
